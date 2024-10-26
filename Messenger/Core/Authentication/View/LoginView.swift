13839cbcb4c0f402545efceb067ac833ff013620
nikammrohit <82987914+nikammrohit@users.noreply.github.com>
2024-10-25 21:33:37 -0500

    Setup files for messenger app

---
+++ Messenger/Core/Authentication/View/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  Messenger
+//
+//  Created by Rohit Nikam on 10/25/24.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
